ff6e1d1f318d31eeeba260cc636fafa1c7cd0f7b
Adrián Kronewitter Rufino <87658936+adkr38@users.noreply.github.com>
2023-01-13 11:10:50 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ const mainLoader = document.querySelector(".main-loader");
 const blurWrapper = document.querySelector(".blur-wrapper");
 blurWrapper.style.display = "none";
 
-// const chatData = ChatRoom.fetchMessages()
-//   .then((data) => {
-//     mainLoader.style.display = "none";
-//     blurWrapper.style.display = "block";
-//
-//     const slider = new Slider();
-//
-//     ChatRoom.addChatListeners(data);
-//
-//     return data;
-//   })
-//   .catch((err) => console.log(err));
+const chatData = ChatRoom.fetchMessages()
+  .then((data) => {
+    mainLoader.style.display = "none";
+    blurWrapper.style.display = "block";
+    
+    const slider = new Slider();
+    ChatRoom.addChatListeners(data);
+
+    return data;
+  }).catch((err) => console.log(err));
